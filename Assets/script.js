75aac38179d42cb1c12c1d96b4d54d8a0106fd54
ziieng <cjengelhardt@gmail.com>
2020-10-04 20:46:51 -0700

    Removed label element from time block objects

--- Assets/script.js
+++ Assets/script.js
@@ -1,71 +1,66 @@
-//get current date/time from Moment, store in a variable
+//get current date/time from Moment
 let now = moment()
+//store current hour for later comparisons
 let nHour = moment().hour()
 let scheduleKeeper = []
 
 //Populate date at top of page - .format(dddd, MMMM Do YYYY)
-$("#currentDay").text(now.format("dddd, MMMM Do YYYY"))
+$("#currentDay").text(now.format("dddd, MMMM Do YYYY h:mm a"))
 
 //Pull existing schedule object out of local storage, or start with some defaults
 if (localStorage.getItem("scheduleStorage")) {
+    //Parse stored array
     scheduleKeeper = JSON.parse(localStorage.getItem("scheduleStorage"))
 } else {
-    //Do I need the label property, or can I just have Moment parse a time to make it?
+    //Load default schedule
     scheduleKeeper = [{
-            label: "9am",
             time: 09,
             text: "Sign in to online systems"
         },
         {
-            label: "10am",
             time: 10,
             text: ""
         },
         {
-            label: "11am",
             time: 11,
             text: ""
         },
         {
-            label: "12pm",
             time: 12,
             text: ""
         },
         {
-            label: "1pm",
             time: 13,
             text: "Lunch!"
         },
         {
-            label: "2pm",
             time: 14,
             text: ""
         },
         {
-            label: "3pm",
             time: 15,
             text: ""
         },
         {
-            label: "4pm",
             time: 16,
             text: ""
         },
         {
-            label: "5pm",
             time: 17,
             text: "Shut down computer"
         }
     ]
+    //Save default schedule
+    localStorage.setItem("scheduleStorage", JSON.stringify(scheduleKeeper))
 }
-//Add rows to page for each hour
+//Add rows to page for each hour, and note row's index in interactive elements
 for (i = 0; i < scheduleKeeper.length; i++) {
     let block = scheduleKeeper[i]
     console.log(block)
     let newRow = $("<row>").addClass("row form-inline col-md-10 mx-auto no-gutters px-0")
     //Add label with block.label
     let newLabel = $("<div>").addClass("col-1 h-100 time-block")
-    newLabel.text(block.label)
+    newLabel.text(moment(block.time, 'HH').format('ha'))
     //Add text field, populate any saved text
     let newArea = $("<textarea>").addClass("col-10 h-100")
     newArea.val(block.text)
@@ -82,7 +77,7 @@ for (i = 0; i < scheduleKeeper.length; i++) {
     //Add save button
     let newButton = $("<button>").addClass("btn col-1 h-100 saveBtn")
     newButton.html('<i class="far fa-save"></i>')
-    //give index data element to button
+    //give index data element to save button
     newButton.attr("data-index", i)
     //Attach elements to row
     newRow.append(newLabel)
@@ -92,7 +87,6 @@ for (i = 0; i < scheduleKeeper.length; i++) {
     $("#planner").append(newRow)
 };
 //On save, update object of relevant time block and localStorage
-
 //click listener for save buttons
 $(document).on("click", ".saveBtn", saveText);
 
